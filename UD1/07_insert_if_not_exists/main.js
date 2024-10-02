f702aea3a7e4c0e4eae6d9223f8404db33d1767e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-02 19:57:00 +0200

    tarea 7 y mitad del 8

--- UD1/07_insert_if_not_exists/main.js
+++ UD1/07_insert_if_not_exists/main.js
@@ -1,14 +1,30 @@
-const assert = require('assert').strict
+
+
 
 function insertIfNotExists(array, item, position) {
+    //si el item se encuentra en el array return lo imprime por pantalla array normal
+    if(!array.includes(item)){
+        //en caso de entrar comprobara la posicion si es true primer, false segundo
+        if(position){
+            array.unshift(item)
+        }else{
+            array.push(item)
+        }
+    }
     return array
 }
 
-
+//lista de arrys 
 let array = ['pera', 'manzana']
+insertIfNotExists(array, 'pera', false)
+insertIfNotExists(array, 'melon', false)
+insertIfNotExists(array, 'melocoton', true)
 
-let result = insertIfNotExists(array, 'pera', false)
-assert.deepStrictEqual(result, ['pera', 'manzana'])
+//imprime por pantalla el arry 
+console.log(array);
+
+insertIfNotExists();
+
 
 // result = insertIfNotExists(array, 'melón', false)
 // assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
--- UD1/08_abs/main.js
+++ UD1/08_abs/main.js
@@ -2,8 +2,10 @@ const assert = require('assert').strict;
 
 function abs(value) {
 
-}
+insertIfNotExists(Num, -4)
+Math.abs(Num)
 
+}
 assert.strictEqual(abs(4), 4)
 assert.strictEqual(abs(-4), 4)
 assert.strictEqual(abs(0), 0)
