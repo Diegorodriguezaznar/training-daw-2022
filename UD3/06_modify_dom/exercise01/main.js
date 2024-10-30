a693f51351ded0f409dd3ec77854516ba18c7754
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-30 20:07:15 +0100

    Subida de tareas hechas 3.6 y 3.7

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,33 +1,50 @@
 window.onload = function(e) {
-    console.log('documento cargado')
+    console.log('documento cargado');
+    document.getElementsByTagName('h2')[1].innerText = 'Changed from JS!';
+    const username = document.getElementById('username');
+    console.log(username);
+    const allFirst = document.querySelectorAll('article .first');
 
-    document.getElementsByTagName('h1')[1].innerText = 'Changed from JS!!'
-   
-    
-
-    const usernameElement = document.getElementById('username');
-    console.log(usernameElement); 
-    
-
-
-    const allFirst = document.querySelectorAll('article .first')
     for (let item of allFirst) {
-        item.computedStyleMap['background-color'] = 'green'
+        item.style['background-color'] = 'red';
+    }
+    const allLi = document.querySelectorAll('li.item');
+
+    for (let item of allLi) {
+    console.log(item);
     }
 
+    const allButtons = document.querySelectorAll('.buttons button');
 
-    const liElement = document.querySelectorAll('li.item')
-    console.log(liElement)
+    document.querySelectorAll('p')[0].style['background-color'] = '#ffccff';
 
-
-    const buttons = document.querySelectorAll('.buttons button')
-    document.querySelectorAll('p')[0].style
-    ['background-color'] = 'ffccff'
-
-
-    for (let item of allButtons) {
+    for (let item of allButtons){
         item.style.color = 'red'
     }
 
+    let firstArticle = document.querySelector('article')
+
+    let node = document.createElement('p')
+    node.innerText = 'Hola mundo añadiendo nodos en el árbol DOM'    
+    node.style['background-color'] = 'yellow'
+    node.title = 'Párrafo de ejemplo'
+
+    let aNode = document.createElement('a')
+    aNode.innerText = 'Enlace de ejemplo'
+    aNode.href= 'https://www.google.es'
+
+    node.appendChild(aNode)
+
+    let fullNode = document.createElement('div')
+    fullNode.innerHTML = `Esto es un div de ejemplo
+    que lleva un link <a href="http://www.marca.es">Marca</a>`
+
+    node.appendChild(fullNode)
+
+    firstArticle.insertBefore(node, firstArticle.childNodes[2])
+
+
+    // Borrar nodos
+    firstArticle.removeChild(firstArticle.childNodes[1])
 
 }
